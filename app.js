7978dcf1416f3be00a41e1b8311af8bfff66fb2f
elhagin <omar.elhagin@live.com>
2019-04-09 15:32:39 +0200

    removed unnecessary items and modified start script

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./controllers/index');
-var usersRouter = require('./controllers/users');
+const indexRouter = require('./controllers/index');
+const usersRouter = require('./controllers/users');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -30,12 +26,11 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = "test msg";
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = "error";
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // // render the error page
-  res.status(200);
-  // res.render('sseee');
+  res.status(err.status || 500);
+  res.json({ 'err': err });
 });
 
 module.exports = app;
