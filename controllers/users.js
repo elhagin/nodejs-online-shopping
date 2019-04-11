07743bfb0696d77cf31283b831d07d764bb4ce26
elhagin <omar.elhagin@live.com>
2019-04-11 16:39:48 +0200

    decrement stock after purchase

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const createError = require('http-errors');
+const createError = require("http-errors");
 
 const usersModel = require("../models/users");
 const productsModel = require("../models/products");
@@ -28,6 +28,14 @@ const authenticate = function(req, res, next) {
 };
 
 const addToCart = function(req, res, next) {
+	// const productIDs = [];
+	// req.body.products.forEach(product => {
+	// 	productIDs.push(product.productID);
+	// })
+
+	// productsModel.find({ _id: { $in: productIDs } }, function(err, products) {
+
+	// });
 	productsModel.findOne({ _id: req.body.productID }, function(
 		err,
 		productData
@@ -38,9 +46,7 @@ const addToCart = function(req, res, next) {
 			const productQuantity = productData.quantity;
 			const purchaseQuantity = req.body.quantity;
 			if (productQuantity >= purchaseQuantity) {
-				updateProduct(req.body.productID, productQuantity - purchaseQuantity);
-				updateUserCart(req.body.userID, productData, purchaseQuantity);
-				res.json({ status: 200, message: "Successfully added to cart" });
+				updateUserCart(req.body.userID, productData, purchaseQuantity, res, next);
 			} else {
 				res.json({
 					status: "error",
@@ -52,32 +58,29 @@ const addToCart = function(req, res, next) {
 	});
 };
 
-function updateProduct(productID, newQuantity) {
-	productsModel.updateOne(
-		{ _id: productID },
-		{ quantity: newQuantity },
-		function(err, res) {
-			if (err) {
-				next(err);
-			}
-		}
-	);
-};
-
-function updateUserCart(userID, productToBeAdded, quantity) {
+function updateUserCart(userID, productToBeAdded, quantity, res, next) {
 	usersModel.findOne({ _id: userID }, function(err, userData) {
 		let alreadyAddedProduct = false;
 		userData.cart.forEach(product => {
-			if (product.productID.equals(productToBeAdded.productID)) {
+			// console.log("product.productID");
+			// console.log(product.productID);
+			// console.log("productToBeAdded.productID");
+			// console.log(productToBeAdded.productID);
+			
+			if (product.productID.equals(productToBeAdded._id)) {
 				alreadyAddedProduct = true;
 				const newQuantity = (product.quantity += quantity);
 
 				usersModel.updateOne(
 					{ _id: userID, "cart.productID": productToBeAdded.productID },
 					{ $set: { "cart.$.quantity": newQuantity } },
-					function(err, res) {
+					function(err, result) {
 						if (err) {
 							next(err);
+						}
+
+						if (result.ok) {
+							res.json({ status: 200, message: "Successfully added to cart" });
 						}
 					}
 				);
@@ -92,17 +95,21 @@ function updateUserCart(userID, productToBeAdded, quantity) {
 				currency: productToBeAdded.currency
 			};
 			usersModel.updateOne(
-				{ _id: userID },
+				{ _id: 'userID' },
 				{ $push: { cart: productToBeAdded } },
-				function(err, res) {
+				function(err, result) {
 					if (err) {
 						next(err);
+					}
+
+					if (result.ok) {
+						res.json({ status: 200, message: "Successfully added to cart" });
 					}
 				}
 			);
 		}
 	});
-};
+}
 
 const proceedWithPayment = function(req, res, next) {
 	if (!req.body.userID) {
@@ -119,39 +126,62 @@ const proceedWithPayment = function(req, res, next) {
 		user.cart.forEach(product => {
 			productsModel.findById(product.productID, function(err, productData) {
 				if (productData.quantity < product.quantity) {
-					next(createError("Product stock is less than required purchase quantity"));
+					next(
+						createError("Product stock is less than required purchase quantity")
+					);
 				}
+
+				decrementProductStock(
+					product.productID,
+					productData.quantity - product.quantity,
+					next
+				);
 			});
 
-			if (product.currency !== 'USD') {
+			if (product.currency !== "USD") {
 				switch (product.currency) {
-					case 'EGP': product.price /= 17.35;
-					break;
+					case "EGP":
+						product.price /= 17.35;
+						break;
 
-					case 'EUR': product.price /= 1.13;
-					break;
+					case "EUR":
+						product.price /= 1.13;
+						break;
 
-					default: next(createError('Only EGP/USD/EUR currencies supported'));
+					default:
+						next(createError("Only EGP/USD/EUR currencies supported"));
 				}
 			}
-			
+
 			totalToBePaid += product.price;
 		});
 
 		const sid = 901405743;
-		const link = `https://sandbox.2checkout.com/checkout/purchase?sid=${sid}&mode=2CO&li_0_price=${totalToBePaid}`
-		
+		const link = `https://sandbox.2checkout.com/checkout/purchase?sid=${sid}&mode=2CO&li_0_price=${totalToBePaid}`;
+
 		res.send(link);
 	});
 };
 
 const purchaseSuccess = function(req, res, next) {
-	if (req.query.credit_card_processed === 'Y') {
+	if (req.query.credit_card_processed === "Y") {
 		res.send("Congratulations on your purchase");
 	} else {
 		next(createError("Error during purchase"));
 	}
 };
+
+function decrementProductStock(productID, newQuantity, next) {
+	productsModel.updateOne(
+		{ _id: productID },
+		{ quantity: newQuantity },
+		function(err, res) {
+			if (err) {
+				next(err);
+			}
+		}
+	);
+}
 
 module.exports = {
 	authenticate,
